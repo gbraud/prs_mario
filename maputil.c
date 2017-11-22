ca42a99a27e6b6757a6d74ac2ac1a390efb88c46
Unknown <gabriel.lorrain@etu.u-bordeaux.fr>
2017-11-22 22:55:51 +0100

    gestion des remplacements des objets

--- maputil.c
+++ maputil.c
@@ -8,6 +8,8 @@
 #include "../include/map.h"
 
 int main(int argc, char * argv[]){
+
+
   int fd=open(argv[1],O_RDWR,0640);
   if(fd==-1){
     exit_with_error("erreur ouverture fd\n");
@@ -63,8 +65,7 @@ int main(int argc, char * argv[]){
   
   
   
-  else {
-  	if(strcmp(argv[2],"--setwidth")==0){
+  else if(strcmp(argv[2],"--setwidth")==0){
   	
   	int w=atoi(argv[3]); //new width
     if(read(fd,&width,sizeof(int))==-1){
@@ -224,37 +225,157 @@ int main(int argc, char * argv[]){
     		}
   		}
     }
-		  else{
-		  	for(int i=0;i<h-height;i++){
-		  		buf=1;
-		  		if(write(new,&buf,sizeof(int))!=sizeof(int)){ //mur de gauche
-		    			exit_with_error("erreur écriture contenu supp \n");
-		  		}
-		  		for(int j=1;j<width;j++){
-		  			buf=-1;
-		  			if(write(new,&buf,sizeof(int))!=sizeof(int)){
-		    			exit_with_error("erreur écriture contenu supp \n");
-		  			}
-		  		}
-		  	}
-		  	for(int k=0;k<height;k++){
-		  		for(int j=0;j<width;j++){
-		  			if(read(fd,&buf,sizeof(int))==-1){
-		      		exit_with_error("erreur lecture contenu\n");
-		    		}
-		    		if(write(new,&buf,sizeof(int))!=sizeof(int)){
-		    			exit_with_error("erreur écriture contenu \n");
-		  			}
-		  		}
-		  	}
+	  else{
+	  	for(int i=0;i<h-height;i++){
+	  		buf=1;
+	  		if(write(new,&buf,sizeof(int))!=sizeof(int)){ //mur de gauche
+	    			exit_with_error("erreur écriture contenu supp \n");
+	  		}
+	  		for(int j=1;j<width;j++){
+	  			buf=-1;
+	  			if(write(new,&buf,sizeof(int))!=sizeof(int)){
+	    			exit_with_error("erreur écriture contenu supp \n");
+	  			}
+	  		}
+	  	}
+	  	for(int k=0;k<height;k++){
+	  		for(int j=0;j<width;j++){
+	  			if(read(fd,&buf,sizeof(int))==-1){
+	      		exit_with_error("erreur lecture contenu\n");
+	    		}
+	    		if(write(new,&buf,sizeof(int))!=sizeof(int)){
+	    			exit_with_error("erreur écriture contenu \n");
+	  			}
+	  		}
+	  	}
+    }	
+  }
+
+
+  else if(strcmp(argv[2],"--setobects")==0){
+    lseek(new,0,SEEK_SET);
+
+    int new_nb_objects = (argc - 3)/6;
+    if(read(fd,&width,sizeof(int))==-1){
+      exit_with_error("erreur lecture width\n");
+    }
+    if(write(new,&width,sizeof(int))!=sizeof(int)){
+       exit_with_error("erreur écriture width\n");
+    }
     
+    if(read(fd,&height,sizeof(int))==-1){ 
+      exit_with_error("erreur lecture height\n");
+    }
+    if(write(new,&height,sizeof(int))!=sizeof(int)){
+       exit_with_error("erreur écriture nouvelle hauteur\n");
+    }
     
-    
-  		}	
-  	}
+    if(read(fd,&nb,sizeof(int))==-1){
+      exit_with_error("erreur lecture nb objets\n");
+    }
+    if(write(new,&new_nb_objects,sizeof(int))!=sizeof(int)){
+      exit_with_error("erreur écriture nb objets\n");
+    }
+
+    for(int i = 3 ; i < argc ; i += 6){
+      char * nom = argv[i];
+      int taille = strlen(nom);
+
+      if(write(new,&taille,sizeof(int))!= sizeof(int)){
+        exit_with_error("erreur écriture taille nom");
+      }
+      if(write(new,nom,sizeof(char)*taille)!= sizeof(char)*taille){
+        exit_with_error("erreur écriture nom");
+      }
+      
+      int frame= atoi(argv[i+1]);
+      if(write(new,&frame,sizeof(int))!= sizeof(int)){
+        exit_with_error("erreur écriture frame");
+      }
+      
+      int solidity;
+      int destructible;
+      int collectible;
+      int generator;
+      
+
+      if(strcmp(argv[i+2],"air")){
+        solidity = MAP_OBJECT_AIR;
+      }
+      else if(strcmp(argv[i+2],"semi-solid")){
+        solidity = MAP_OBJECT_SEMI_SOLID;
+      }
+      else if(strcmp(argv[i+2],"solid")){
+        solidity = MAP_OBJECT_SOLID;
+      }
+      else if(strcmp(argv[i+2],"liquid")){
+        solidity = MAP_OBJECT_LIQUID;
+      }
+      else{
+        exit_with_error("erreur écriture solidité de l'objet");
+      }
+
+
+
+      if(strcmp(argv[i+3],"destructible")){
+        destructible = 1;
+      }
+      else if(strcmp(argv[i+3],"not-destructible")){
+        destructible = 0;
+      }
+      else{
+        exit_with_error("erreur écriture destruct de l'objet");
+      }
+
+
+
+      if(strcmp(argv[i+4],"collectible")){
+        collectible = 1;
+      }
+      else if(strcmp(argv[i+4],"not-collectible")){
+        collectible = 0;
+      }
+      else{
+        exit_with_error("erreur écriture collect de l'objet");
+      }
+
+
+
+      if(strcmp(argv[i+5],"generator")){
+        generator = 1;
+      }
+      else if(strcmp(argv[i+5],"not-generator")){
+        generator = 0;
+      }
+      else{
+        exit_with_error("erreur écriture gener de l'objet");
+      }
+
+
+      int prop=solidity
+                  |destructible << 6
+                  |collectible<<7
+                  |generator<<8;
+
+      if(write(new,&prop,sizeof(int))!= sizeof(int)){
+        exit_with_error("erreur écriture propriétés");
+      }
+    }
+
+    lseek(fd,-(sizeof(int)*height*width),SEEK_END);
+    for(int i = 0 ; i < height*width ; i++){
+      if(read(fd,&buf,sizeof(int))==-1){ 
+        exit_with_error("erreur lecture contenu de la map\n");
+      }
+      if(write(new,&buf,sizeof(int))!=sizeof(int)){
+         exit_with_error("erreur écriture contenu de la map\n");
+      }
+    }
+  }
+
   remove("../maps/saved.map");
   rename("new.map", "../maps/saved.map");
-  }
+
   close(new);
   close(fd);
   return 0;
